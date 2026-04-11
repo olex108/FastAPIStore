6c000696e793fd0393f3211af58f080ae7bc04e4
Alex <oleksiymosiychuk108@gmail.com>
2026-04-12 02:03:02 +0300

    feat [models/base.py] : add models conventions

--- src/models/base.py
+++ src/models/base.py
@@ -1,18 +1,35 @@
-from typing import Annotated
 from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 
-class ModelsAnnotated:
+class ModelsAnnotation:
+    """Кастомные настройки аннотации полей в моделях"""
+
     created_at = Annotated[datetime, mapped_column(server_default=datetime.now())]
     updated_at = Annotated[datetime, mapped_column(server_default=datetime.now(), onupdate=datetime.now())]
 
 
-# Создание базового класса
+# Создание базового класса для наследования моделями
 class Base(DeclarativeBase):
-    pass
+    """Базовый класс для наследования моделями"""
+
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
+# Импортируем модели для инициализации при миграциях
+from .cart import Cart
 from .product import Product
 from .user import User
-from .cart import Cart
