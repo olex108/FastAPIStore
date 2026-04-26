589b89264df39f563ee39b99ec87420fc22a2195
Alex <oleksiymosiychuk@gmail.com>
2026-04-26 23:27:00 +0300

    feat [product] : add pagination

--- src/routers/product.py
+++ src/routers/product.py
@@ -1,7 +1,7 @@
 # routers/product.py
-from typing import List, Annotated
+from typing import List, Annotated, Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.dependencies.permissions import PermissionChecker
@@ -13,19 +13,49 @@ from src.crud.product import (
     get_product_by_id,
     update_product_by_id,
 )
-from src.schemas.product import CreateProduct, ProductOut
+from src.schemas.product import CreateProduct, ProductOut, ProductPaginationOut
+from src.config.settings import get_settings
+from sqlalchemy import select
+from src.models.product import Product
+
+
+
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
 @router.get("/", name="products", response_model=List[ProductOut])
-async def get_products_list(
-    current_user: Annotated[str, Depends(PermissionChecker(["products:view"]))],
+async def get_products_paginated_list(
+    limit: Annotated[int, Query(**get_settings().pagination.model_dump())],
+    cursor: Annotated[Optional[str], Query(None, description="price_id")],
+    # sort_by: Annotated[str, Query("price_asc", regex="^(price_asc|price_desc|name_asc)$")],
+    # name_query: Annotated[Optional[str], None],
+    # current_user: Annotated[str, Depends(PermissionChecker(["products:view"]))],
     session: Annotated[AsyncSession, Depends(db_handler.session_getter)],
 ):
 
+    query = select(Product).order_by(Product.id).limit(limit)
+
+    if cursor is not None:
+        prod_price, prod_id = cursor.split("_")
+        query.where(Product.id>prod_id)
+
+    prod_list = await session.execute(query)
+    prod_list = prod_list.scalars().all()
+
+
     product_list = await get_all_products(session=session)
     return product_list
+
+
+# @router.get("/", name="products", response_model=List[ProductOut])
+# async def get_products_list(
+#     current_user: Annotated[str, Depends(PermissionChecker(["products:view"]))],
+#     session: Annotated[AsyncSession, Depends(db_handler.session_getter)],
+# ):
+#
+#     product_list = await get_all_products(session=session)
+#     return product_list
 
 
 @router.get("/{product_id}", name="product", response_model=ProductOut)
