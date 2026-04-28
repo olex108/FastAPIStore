4b9fad5f70fce0c6b544b5009bceea607dd00942
Alex <oleksiymosiychuk108@gmail.com>
2026-04-28 18:28:26 +0300

    feat [mailing] : add mailing task

--- tests/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta, timezone
-from src.config.settings import get_settings
-import jwt
-
-
-def create_test_auth_token(user_email: str):
-    """Генерирует реальный JWT токен для тестов"""
-    payload = {"sub": user_email, "exp": datetime.now(timezone.utc) + timedelta(minutes=15)}
-    return jwt.encode(payload, get_settings().SECRET_KEY, algorithm=get_settings().ALGORITHM)
